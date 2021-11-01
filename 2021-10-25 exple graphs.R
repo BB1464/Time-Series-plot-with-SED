3d7e50f8da0f39e5a1f03259b10f3771d9ff494e
Oluwafemi Oyedele <oluwafemioyedele908@gmail.com>
2021-10-31 21:55:20 -1100

    ribbon

--- 2021-10-25 exple graphs.R
+++ 2021-10-25 exple graphs.R
@@ -7,7 +7,7 @@ quickleaf=read.csv('2021-08-06 leaf biomass_copy paste.csv')
 quickleaf=quickleaf %>% mutate_if(is.character,as.factor)
 
 ggplot(quickleaf,aes(reorder(Month,Order),Avg,group=Treatment))+  #2 methods compared
-  # geom_ribbon(data=quickleaf,aes(ymin=Avg-SE,ymax=Avg+SE,fill=Treatment),alpha=0.1)+
+  geom_ribbon(data=quickleaf,aes(ymin=Avg-SE,ymax=Avg+SE,fill=Treatment),alpha=0.1)+
   geom_line(aes(col=Treatment),linejoin='round',size=0.8)+
   geom_point(aes(col=Treatment),size=3,shape=16)+
   geom_errorbar(data=quickleaf,aes(ymin=max(Avg+SE)-SED,ymax=max(Avg+SE)),col='grey30',
